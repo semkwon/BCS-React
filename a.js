6f64c94beacb7f3049427aed3abe1c31b6582cba
권세명 <3pkwon@gmail.com>
2023-04-10 11:54:28 +0900

    react-icon AM

---
+++ a.js
@@ -0,0 +1,15 @@
+async function checkPromise() {
+  try {
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => resolve("완료!"), 1000);
+    });
+
+    let result = await promise;
+
+    console.log(result22);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+checkPromise();
