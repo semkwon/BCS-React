bdc7ac6ea5057e3d4eef198bac0476984e58904f
권세명 <3pkwon@gmail.com>
2023-04-11 10:03:52 +0900

    router 연결 완료

--- express2/app.js
+++ express2/app.js
@@ -1,9 +1,12 @@
 // CRUD 라우팅 시작! : routes라는 폴더 내에 tweet.js, user.js 생성
 const express = require("express");
+const userRouter = require("./routes/user");
 
 const app = express();
 
 const port = 3010;
+
+app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
