9a40f0204bd7c894dc01558c1341cda6e8816208
권세명 <3pkwon@gmail.com>
2023-04-11 14:46:40 +0900

    done 추가

--- todolist-noDB/backend/app.js
+++ todolist-noDB/backend/app.js
@@ -6,33 +6,12 @@ const app = express();
 const port = 3010;
 
 app.use(express.json()); //json 형식을 읽어오는 방법(미들웨어?)
-
 app.use("/todo", todoRouter);
 
-app.post("/", (req, res) => {
-  res.send("투두 생성 🎉");
-});
-
 app.get("/", (req, res) => {
-  res.send("전체 투두리스트");
-});
-
-app.get("/:id", (req, res) => {
-  res.send("특정 투두리스트");
-});
-
-app.put("/:id", (req, res) => {
-  res.send("투두 수정");
-});
-
-app.put("/done/:id", (req, res) => {
-  res.send("완료여부");
-});
-
-app.delete("/:id", (req, res) => {
-  res.send("투두 삭제");
+  res.send("Hello, Express");
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on port: ${port} 🚀🚀🚀`);
+  console.log(`Server listening on port: ${port} 🦻`);
 });
--- todolist-noDB/backend/routes/todo.js
+++ todolist-noDB/backend/routes/todo.js
@@ -21,4 +21,22 @@ router.post("/", (req, res) => {
   res.json(todoData); // json에 데이터 넣기
 });
 
+//done
+// 주소로 오는건 params
+router.put("/done/:id", (req, res) => {
+  const { id } = req.params;
+
+  todoData[parseInt(id)] = {
+    title: todoData[parseInt(id)].title,
+    desc: todoData[parseInt(id)].desc,
+    //json 배열이라서 id 0으로 하시면 0번방 의 isdone이 바뀐다
+    // 느낌표 사용해서 true, false를 바꿔준다
+    isDone: !todoData[parseInt(id)].isDone,
+  };
+
+  console.log(todoData);
+
+  res.json(todoData);
+});
+
 module.exports = router;
