e67f46eea5b49734f639e04fdcf5dcb093c3eb16
권세명 <3pkwon@gmail.com>
2023-04-13 10:32:50 +0900

    CreateToDo

--- todolist-noDB/frontend/src/App.jsx
+++ todolist-noDB/frontend/src/App.jsx
@@ -1,6 +1,32 @@
-import TodoCard from "./components/TodoCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ToDoCard from "./components/ToDoCard";
+import CreateToDo from "./components/CreateToDo";
 
 function App() {
+  const [toDoList, setToDoList] = useState();
+
+  const getToDoList = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_BACKEND_URL}/todo`
+      );
+
+      if (response.status !== 200) {
+        alert("에러 발생!");
+        return;
+      }
+
+      setToDoList(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getToDoList();
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col justify-start items-center pt-16">
       <h1 className="text-4xl font-bold">AWESOME TO DO LIST 😎</h1>
@@ -13,20 +39,14 @@ function App() {
           나무 베는데 한 시간이 주어진다면, 도끼를 가는데 45분을 쓰겠다,
           에비브러햄 링컨
         </div>
-        <form className="flex mt-2">
-          <input
-            className="grow border-2 border-pink-200 rounded-lg focus:outline-pink-400 px-2 py-1 text-lg"
-            type="text"
-          />
-          <input
-            className="ml-4 px-2 py-1 bg-pink-400 rounded-lg text-gray-50"
-            type="submit"
-            value="새 투두 생성"
-          />
-        </form>
+        <CreateToDo />
       </div>
       <div className="mt-16 flex flex-col w-1/2">
-        <TodoCard />
+        {toDoList
+          ? toDoList.map((v, i) => {
+              return <ToDoCard key={i} title={v.title} />;
+            })
+          : "로딩중입니다..."}
       </div>
     </div>
   );
--- todolist-noDB/frontend/src/components/TodoCard.jsx
+++ todolist-noDB/frontend/src/components/TodoCard.jsx
@@ -1,26 +1,33 @@
 import { useState } from "react";
 
-const TodoCard = () => {
+const ToDoCard = ({ title }) => {
   const [isDone, setIsDone] = useState(false);
+
+  const onClickToggle = () => {
+    setIsDone(!isDone);
+  };
 
   return (
     <div className="flex my-4">
       {isDone ? (
         <>
-          <div className="relative">
+          <button className="relative" onClick={onClickToggle}>
             <div className="border-4 border-pink-400 w-8 h-8 rounded-xl bg-pink-400 p-2"></div>
             <div className="absolute border-4 border-white top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-8 h-8 scale-75 rounded-xl bg-pink-400 p-2"></div>
-          </div>
-          <div className="text-2xl ml-4">👕 빨래하기</div>
+          </button>
+          <div className="text-2xl ml-4">{title}</div>
         </>
       ) : (
         <>
-          <div className="border-4 border-pink-400 w-8 h-8 rounded-xl"></div>
-          <div className="text-2xl ml-4">🧹 청소하기</div>
+          <button
+            className="border-4 border-pink-400 w-8 h-8 rounded-xl"
+            onClick={onClickToggle}
+          ></button>
+          <div className="text-2xl ml-4">{title}</div>
         </>
       )}
     </div>
   );
 };
 
-export default TodoCard;
+export default ToDoCard;
