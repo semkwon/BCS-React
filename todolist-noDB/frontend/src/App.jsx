7d06449293e8a39f3191cb03b1afa12ad18749d5
권세명 <3pkwon@gmail.com>
2023-04-13 09:14:41 +0900

    2023.04.13 reset ready

--- todolist-noDB/frontend/src/App.jsx
+++ todolist-noDB/frontend/src/App.jsx
@@ -1,35 +1,6 @@
-import axios from "axios"; // 새 터미널에서 npm i axios하고 import로 불러오기
-import TodoCard from "./components/TodoCard";
-import { useEffect, useState } from "react";
-import CreateToDo from "./components/CreateToDo";
-
 function App() {
-  const [toDoList, setToDoList] = useState();
-
-  const getToDoList = async () => {
-    try {
-      //axios 요청
-      const response = await axios.get(
-        `${process.env.REACT_APP_BACKEND_URL}/todo`
-      );
-
-      if (response.status !== 200) {
-        alert("요청을 불러오지 못했습니다.");
-        return;
-      }
-
-      setToDoList(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    getToDoList();
-  }, []);
-
   return (
-    <div className="min-h-screen bg-blue-200 flex flex-col justify-start items-center pt-16">
+    <div className="min-h-screen flex flex-col justify-start items-center pt-16">
       <h1 className="text-4xl font-bold">AWESOME TO DO LIST 😎</h1>
       <div>
         <div className="mt-8 text-sm font-semibold">
@@ -40,13 +11,30 @@ function App() {
           나무 베는데 한 시간이 주어진다면, 도끼를 가는데 45분을 쓰겠다,
           에비브러햄 링컨
         </div>
-        <CreateToDo getToDoList={getToDoList} />
-        <ul className="mt-16 flex flex-col w-1/2">
-          {toDoList &&
-            toDoList.map((v, i) => {
-              return <TodoCard key={i} title={v.title} />;
-            })}
-        </ul>
+        <form className="flex mt-2">
+          <input
+            className="grow border-2 border-pink-200 rounded-lg focus:outline-pink-400 px-2 py-1 text-lg"
+            type="text"
+          />
+          <input
+            className="ml-4 px-2 py-1 bg-pink-400 rounded-lg text-gray-50"
+            type="submit"
+            value="새 투두 생성"
+          />
+        </form>
+      </div>
+      <div className="mt-16 flex flex-col w-1/2">
+        <div className="flex my-4">
+          <div className="border-4 border-pink-400 w-8 h-8 rounded-xl"></div>
+          <div className="text-2xl ml-4">🧹 청소하기</div>
+        </div>
+        <div className="flex my-4">
+          <div className="relative">
+            <div className="border-4 border-pink-400 w-8 h-8 rounded-xl bg-pink-400 p-2"></div>
+            <div className="absolute border-4 border-white top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-8 h-8 scale-75 rounded-xl bg-pink-400 p-2"></div>
+          </div>
+          <div className="text-2xl ml-4">👕 빨래하기</div>
+        </div>
       </div>
     </div>
   );
