5b75c93574264f64e6f4c420689deb9a48d124ef
권세명 <3pkwon@gmail.com>
2023-04-14 10:18:14 +0900

    todoRouter 생성

--- TODOLIST/backend/app.js
+++ TODOLIST/backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const userRouter = require("./routes/user");
+const todoRouter = require("./routes/todo");
 
 const app = express();
 
@@ -9,6 +10,7 @@ const port = 3010;
 app.use(cors());
 app.use(express.json());
 app.use("/user", userRouter);
+app.use("/todo", todoRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
