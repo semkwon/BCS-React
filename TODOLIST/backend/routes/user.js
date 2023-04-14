cb66ff05b0fd838867cc0d9ed2c7fa73c451a417
권세명 <3pkwon@gmail.com>
2023-04-14 10:08:04 +0900

    something..

--- TODOLIST/backend/routes/user.js
+++ TODOLIST/backend/routes/user.js
@@ -10,6 +10,17 @@ router.post("/", async (req, res) => {
   try {
     const { account } = req.body;
 
+    const existUser = await client.user.findUnique({
+      where: {
+        account,
+      },
+    });
+    if (existUser) {
+      return res
+        .status(400)
+        .json({ ok: false, error: "Already exist account." });
+    }
+
     const user = await client.user.create({
       data: {
         account,
@@ -17,6 +28,33 @@ router.post("/", async (req, res) => {
     });
 
     res.json({ ok: true, user });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// 유저조회
+router.get("/:account", async (req, res) => {
+  try {
+    const { account } = req.params;
+
+    const user = await client.user.findUnique({
+      where: {
+        account,
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        error: "Not exist user.",
+      });
+    }
+
+    res.json({
+      ok: true,
+      user,
+    });
   } catch (error) {
     console.error(error);
   }
