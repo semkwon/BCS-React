fb9e8ea9364029dda8db6e9689f0110e74c901e9
권세명 <3pkwon@gmail.com>
2023-04-14 15:40:33 +0900

    createUser useState

--- TODOLIST/frontend/src/components/LogIn.jsx
+++ TODOLIST/frontend/src/components/LogIn.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 const LogIn = () => {
+  const [createUser, setCreateUser] = useState("");
+
   return (
     <div className="min-h-screen flex flex-col justify-center items-center">
       <form className="flex mt-2 my-16">
         <input
           className="grow border-2 border-pink-200 rounded-lg focus:outline-pink-400 px-2 py-1 text-lg"
           type="text"
+          value={createUser}
+          onChange={(e) => setCreateUser(e.target.value)}
         />
         <input
           className="ml-4 px-2 py-1 bg-pink-400 rounded-lg text-gray-50 w-24"
