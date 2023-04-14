0e8f0f46be1d869239530c5b42c5ef71f111eb5a
권세명 <3pkwon@gmail.com>
2023-04-14 13:57:14 +0900

    투두 완료 & 삭제

--- TODOLIST/backend/routes/todo.js
+++ TODOLIST/backend/routes/todo.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const client = new PrismaClient();
 
-// 1. 투두 생성
+// 투두 생성
 router.post("/", async (req, res) => {
   try {
     const { todo, userId } = req.body;
@@ -41,13 +41,96 @@ router.post("/", async (req, res) => {
   }
 });
 
-// 2. 투두 조회
-// router.get
+// 투두 조회
+router.get("/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
 
-// 3. 투두 완료(수정)
-// router.put
+    const user = await client.user.findUnique({
+      where: {
+        id: parseInt(userId),
+      },
+    });
 
-// 4. 투두 삭제
-// router.delete
+    if (!user) {
+      return res.status(400).json({ ok: false, error: "Not exist user." });
+    }
+
+    const todos = await client.todo.findMany({
+      where: {
+        userId: parseInt(userId),
+      },
+    });
+
+    res.json({ ok: true, todos });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// 투두 완료 (수정)
+router.put("/:id/done", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+
+    const existTodo = await client.todo.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    if (!existTodo) {
+      return res.status(400).json({ ok: false, error: "Not exist todo." });
+    }
+    if (existTodo.userId !== parseInt(userId)) {
+      return res.status(400).json({ ok: false, error: "U R not todo owner." });
+    }
+
+    const updatedTodo = await client.todo.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        isDone: !existTodo.isDone,
+      },
+    });
+
+    res.json({ ok: true, todo: updatedTodo });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// 투두 삭제
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+
+    const existTodo = await client.todo.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    if (!existTodo) {
+      return res.status(400).json({ ok: false, error: "Not exist todo." });
+    }
+    if (existTodo.userId !== parseInt(userId)) {
+      return res.status(400).json({ ok: false, error: "U R not todo owner." });
+    }
+
+    const deletedTodo = await client.todo.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    res.json({ ok: true, todo: deletedTodo });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 module.exports = router;
