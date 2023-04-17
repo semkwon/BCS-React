a4e890fedd7dc5df39bb3adec16937076f4353fa
권세명 <3pkwon@gmail.com>
2023-04-17 13:46:59 +0900

    최신 todoData 요청 - 전체를 불러와 데이터 낭비

---
+++ TODOLIST/frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import axios from "axios";
+
+const CreateTodo = ({ userId, getTodos }) => {
+  const [todo, setTodo] = useState("");
+
+  const onSubmitCreateTodo = async (e) => {
+    try {
+      e.preventDefault();
+
+      await axios.post(`${process.env.REACT_APP_BACKEND_URL}/todo`, {
+        todo,
+        userId,
+      });
+
+      getTodos();
+      setTodo("");
+    } catch (error) {
+      console.error(error);
+
+      alert("투두 생성중 에러가 발생하였습니다.");
+    }
+  };
+
+  return (
+    <form className="flex mt-2" onSubmit={onSubmitCreateTodo}>
+      <input
+        className="grow border-2 border-pink-200 rounded-lg focus:outline-pink-400 px-2 py-1 text-lg"
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <input
+        className="ml-4 px-2 py-1 bg-pink-400 rounded-lg text-gray-50"
+        type="submit"
+        value="새 투두 생성"
+      />
+    </form>
+  );
+};
+
+export default CreateTodo;
