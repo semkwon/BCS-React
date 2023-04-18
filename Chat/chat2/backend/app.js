29d12a9dcb16b1758c12c34786ddb8f9da517001
semkwon <3pkwon@gmail.com>
2023-04-18 14:03:40 +0900

    chat1

--- Chat/chat2/backend/app.js
+++ Chat/chat2/backend/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
@@ -12,6 +13,43 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
+});
+
+app.post("/chat", async (req, res) => {
+  try {
+    const { content } = req.body;
+
+    const bearerToken = req.headers.authorization?.substring(7);
+
+    if (bearerToken !== process.env.SECRET_KEY) {
+      return res
+        .status(400)
+        .json({ ok: false, error: "올바른 키를 입력해주세요." });
+    }
+    if (!content) {
+      return res.status(400).json({ ok: false, error: "질문을 입력해주세요." });
+    }
+
+    const response = await axios.post(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content }],
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_KEY}`,
+        },
+      }
+    );
+
+    console.log(response);
+
+    res.send("임시");
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.listen(port, () => {
