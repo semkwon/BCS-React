179e206847e3f31405fe3f8c32e8a17539f2bfdd
semkwon <3pkwon@gmail.com>
2023-04-20 16:19:16 +0900

    setMyBalance

--- BLOCKCHAIN2/src/App.jsx
+++ BLOCKCHAIN2/src/App.jsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Web3 from "web3";
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from "./web3.config";
 
 const web3 = new Web3("https://rpc-mumbai.maticvigil.com");
+const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
 
 function App() {
   const [account, setAccount] = useState("");
+  const [myBalance, setMyBalance] = useState();
 
   const onClickAccount = async () => {
     try {
@@ -20,20 +23,35 @@ function App() {
   const onClickLogOut = () => {
     setAccount("");
   };
+  const onClickBalance = async () => {
+    try {
+      if (!account || !contract) return;
 
-  useEffect(() => {
-    console.log(web3);
-  }, []);
+      const balance = await contract.methods.balanceOf(account).call();
+
+      setMyBalance(balance);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="bg-red-100 min-h-screen flex justify-center items-center">
       {account ? (
-        <div className="text-main font-semibold text-2xl">
-          {account.substring(0, 4)}...
-          {account.substring(account.length - 4)}
-          <button className="ml-4 btn-style" onClick={onClickLogOut}>
-            로그아웃
-          </button>
+        <div>
+          <div className="text-main font-semibold text-2xl">
+            {account.substring(0, 4)}...
+            {account.substring(account.length - 4)}
+            <button className="ml-4 btn-style" onClick={onClickLogOut}>
+              로그아웃
+            </button>
+          </div>
+          <div className="flex items-center mt-4">
+            {myBalance && <div>{myBalance}</div>}
+            <button className="ml-2 btn-style" onClick={onClickBalance}>
+              잔액 조회
+            </button>
+          </div>
         </div>
       ) : (
         <button className="btn-style" onClick={onClickAccount}>
