5fe751848744677c4b459678d4253d548c5feebb
semkwon <3pkwon@gmail.com>
2023-04-20 13:46:27 +0900

    Meta connect

--- BLOCKCHAIN2/src/App.jsx
+++ BLOCKCHAIN2/src/App.jsx
@@ -1,5 +1,26 @@
 function App() {
-  return <div className="bg-red-100">Hello, React</div>;
+  const onClickAccount = async () => {
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log(accounts);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="bg-red-100 min-h-screen flex justify-center items-center">
+      <button className="btn-style" onClick={onClickAccount}>
+        <img
+          className="w-12"
+          src={`${process.env.PUBLIC_URL}/images/metamask.png`}
+        />
+      </button>
+    </div>
+  );
 }
 
 export default App;
