d032301353535fd7773d4be36c4b19ee690555e3
semkwon <3pkwon@gmail.com>
2023-04-20 14:38:50 +0900

    Logout button

--- BLOCKCHAIN2/src/App.jsx
+++ BLOCKCHAIN2/src/App.jsx
@@ -1,24 +1,41 @@
+import { useState } from "react";
+
 function App() {
+  const [account, setAccount] = useState("");
+
   const onClickAccount = async () => {
     try {
       const accounts = await window.ethereum.request({
         method: "eth_requestAccounts",
       });
 
-      console.log(accounts);
+      setAccount(accounts[0]);
     } catch (error) {
       console.error(error);
     }
   };
+  const onClickLogOut = () => {
+    setAccount("");
+  };
 
   return (
     <div className="bg-red-100 min-h-screen flex justify-center items-center">
-      <button className="btn-style" onClick={onClickAccount}>
-        <img
-          className="w-12"
-          src={`${process.env.PUBLIC_URL}/images/metamask.png`}
-        />
-      </button>
+      {account ? (
+        <div className="text-main font-semibold text-2xl">
+          {account.substring(0, 4)}...
+          {account.substring(account.length - 4)}
+          <button className="ml-4 btn-style text" onClick={onClickLogOut}>
+            로그아웃
+          </button>
+        </div>
+      ) : (
+        <button className="btn-style" onClick={onClickAccount}>
+          <img
+            className="w-12"
+            src={`${process.env.PUBLIC_URL}/images/metamask.png`}
+          />
+        </button>
+      )}
     </div>
   );
 }
