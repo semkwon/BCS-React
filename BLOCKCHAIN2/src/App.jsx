8a219d17900642a7e8f64380c91af763a52d0f2e
semkwon <3pkwon@gmail.com>
2023-04-20 15:09:54 +0900

    web3

--- BLOCKCHAIN2/src/App.jsx
+++ BLOCKCHAIN2/src/App.jsx
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Web3 from "web3";
+
+const web3 = new Web3("https://rpc-mumbai.maticvigil.com");
 
 function App() {
   const [account, setAccount] = useState("");
@@ -18,13 +21,17 @@ function App() {
     setAccount("");
   };
 
+  useEffect(() => {
+    console.log(web3);
+  }, []);
+
   return (
     <div className="bg-red-100 min-h-screen flex justify-center items-center">
       {account ? (
         <div className="text-main font-semibold text-2xl">
           {account.substring(0, 4)}...
           {account.substring(account.length - 4)}
-          <button className="ml-4 btn-style text" onClick={onClickLogOut}>
+          <button className="ml-4 btn-style" onClick={onClickLogOut}>
             로그아웃
           </button>
         </div>
