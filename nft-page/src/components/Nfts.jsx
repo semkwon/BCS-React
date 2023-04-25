043f1142184c9fea5ed5a896afb46d2e770022e5
semkwon <3pkwon@gmail.com>
2023-04-25 17:59:13 +0900

    page half done

--- nft-page/src/components/Nfts.jsx
+++ nft-page/src/components/Nfts.jsx
@@ -1,37 +1,67 @@
-import { useState } from "react";
+import Web3 from "web3";
+import Intro from "../components/Intro";
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from "../web3.config";
+import { useEffect, useState } from "react";
+import Nfts from "../components/Nfts";
 
-const Nfts = ({ page }) => {
-  const [selectedPage, setSelectedPage] = useState(1);
+const web3 = new Web3(window.ethereum);
+const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
 
-  const onClickPage = (p) => () => {
-    setSelectedPage(p);
-  };
+const Main = ({ account }) => {
+  const [totalNft, setTotalNft] = useState(0);
+  const [mintedNft, setMintedNft] = useState(0);
+  const [myNft, setMyNft] = useState(0);
+  const [page, setPage] = useState(1);
 
-  const pageComp = () => {
-    let pageArray = [];
+  const getTotalNft = async () => {
+    try {
+      if (!contract) return;
 
-    for (let i = 0; i < page; i++) {
-      pageArray.push(
-        <button
-          key={i}
-          className={`ml-4 text-2xl font-bold hover:text-white ${
-            i + 1 === selectedPage ? "text-white" : "text-gray-400"
-          }`}
-          onClick={onClickPage(i + 1)}
-        >
-          {i + 1} <span className="text-base">페이지</span>
-        </button>
-      );
+      const response = await contract.methods.totalNft().call();
+
+      setTotalNft(response);
+    } catch (error) {
+      console.error(error);
     }
-
-    return pageArray;
   };
+  const getMintedNft = async () => {
+    try {
+      if (!contract) return;
+
+      const response = await contract.methods.totalSupply().call();
+
+      setMintedNft(response);
+      setPage(parseInt((parseInt(response) - 1) / 10) + 1);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const getMyNft = async () => {
+    try {
+      if (!contract || !account) return;
+
+      const response = await contract.methods.balanceOf(account).call();
+
+      setMyNft(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getTotalNft();
+    getMintedNft();
+  }, []);
+  useEffect(() => {
+    getMyNft();
+  }, [account]);
 
   return (
-    <div>
-      <div>{pageComp()}</div>
-    </div>
+    <>
+      <Intro totalNft={totalNft} mintedNft={mintedNft} myNft={myNft} />
+      <Nfts page={page} />
+    </>
   );
 };
 
-export default Nfts;
+export default Main;
