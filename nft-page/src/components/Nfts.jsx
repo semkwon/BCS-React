a5caa7d940c7c81cd84c5a02b3d662ab471b837d
semkwon <3pkwon@gmail.com>
2023-04-25 17:02:33 +0900

    페이지

--- nft-page/src/components/Nfts.jsx
+++ nft-page/src/components/Nfts.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 const Nfts = ({ page }) => {
+  const [selectedPage, setSelectedPage] = useState(1);
+
   const pageComp = () => {
     let pageArray = [];
 
     for (let i = 0; i < page; i++) {
       pageArray.push(
-        <button key={i}>
-          {i + 1} <span>페이지</span>
+        <button
+          key={i}
+          className={`ml-4 text-2xl font-bold hover:text-white ${
+            i + 1 === selectedPage ? "text-white" : "text-gray-400"
+          }`}
+        >
+          {i + 1} <span className="text-base">페이지</span>
         </button>
       );
     }
