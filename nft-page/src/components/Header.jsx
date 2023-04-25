516b49fc7ff0377268ea50a2825c3c183c4b4df7
semkwon <3pkwon@gmail.com>
2023-04-25 11:27:23 +0900

    home 구현

--- nft-page/src/components/Header.jsx
+++ nft-page/src/components/Header.jsx
@@ -1,5 +1,17 @@
+import { ImHome } from "react-icons/im";
+import { Link } from "react-router-dom";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="max-w-screen-xl mx-auto p-4 flex justify-between font-bold">
+      <Link to="/">
+        <div className="flex items-center text-main">
+          <ImHome size={28} />
+          <div className="ml-4 text-xl">Block Chain School</div>
+        </div>
+      </Link>
+      <div>wallet</div>
+    </header>
+  );
 };
 
 export default Header;
