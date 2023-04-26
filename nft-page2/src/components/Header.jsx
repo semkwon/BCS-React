c26b3b6497262cefb7dd947674d0d1318c3daac6
semkwon <3pkwon@gmail.com>
2023-04-26 13:49:14 +0900

    account

--- nft-page2/src/components/Header.jsx
+++ nft-page2/src/components/Header.jsx
@@ -1,8 +1,31 @@
-const Header = () => {
+const Header = ({ account, setAccount }) => {
+  const onClickAccount = async () => {
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      setAccount(accounts[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <header className="max-w-screen-xl mx-auto p-4 flex justify-between font-bold">
       <div className="ml-1 text-xl">Ble-Chess</div>
-      <div className="flex items-center"></div>
+      <div className="flex items-center">
+        {account ? (
+          account
+        ) : (
+          <button
+            className="p-2 bg-gray-800 rounded-full ml-1 text-white"
+            onClick={onClickAccount}
+          >
+            Connect
+          </button>
+        )}
+      </div>
     </header>
   );
 };
