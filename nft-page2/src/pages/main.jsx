1b965f609a6de25d507e000f79abb4bb6c4a6b33
semkwon <3pkwon@gmail.com>
2023-04-26 14:47:16 +0900

    total NFT

--- nft-page2/src/pages/main.jsx
+++ nft-page2/src/pages/main.jsx
@@ -1,6 +1,33 @@
+import Web3 from "web3";
 import Intro from "../components/Intro";
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from "../web3.config";
+import { useEffect, useState } from "react";
 
+const web3 = new Web3(window.ethereum);
+const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+
+console.log(contract);
 const Main = () => {
+  const [totalNft, setTotalNft] = useState(0);
+
+  const getTotalNft = async () => {
+    try {
+      if (!contract) return;
+
+      const response = await contract.methods.totalNft().call();
+
+      console.log(response);
+
+      setTotalNft(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getTotalNft();
+  }, []);
+
   return (
     <div>
       <Intro />
