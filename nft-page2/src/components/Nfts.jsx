5243078221284531eb30a2213c20cfe3dc04a833
semkwon <3pkwon@gmail.com>
2023-04-26 15:39:29 +0900

    페이지 선택 구현중

--- nft-page2/src/components/Nfts.jsx
+++ nft-page2/src/components/Nfts.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
+
 const Nfts = ({ page }) => {
+  const [selectedPage, setSelectedPage] = useState(1);
+
   const pageComp = () => {
     let pageArray = [];
 
     for (let i = 0; i < page; i++) {
-      pageArray.push(<button key={i}>{i + 1} 페이지</button>);
+      pageArray.push(
+        <button
+          className={`${i !== 0 && "ml-4"} ${
+            i + 1 === selectedPage ? "text-black" : "text-gray-500"
+          }`}
+          key={i}
+        >
+          {i + 1} 페이지
+        </button>
+      );
     }
     return pageArray;
   };
