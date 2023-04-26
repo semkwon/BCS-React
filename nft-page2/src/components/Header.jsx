7d647391c0c65dcab05582e6338a202f5a1445aa
semkwon <3pkwon@gmail.com>
2023-04-26 13:41:09 +0900

    Header

---
+++ nft-page2/src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="max-w-screen-xl mx-auto p-4 flex justify-between font-bold">
+      <div className="ml-1 text-xl">Ble-Chess</div>
+      <div className="flex items-center"></div>
+    </header>
+  );
+};
+
+export default Header;
