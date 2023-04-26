d2763597d1245039aa0001017d0fa042b2339130
semkwon <3pkwon@gmail.com>
2023-04-26 15:52:29 +0900

    10개씩 정보 가져오기

--- nft-page2/src/components/Nfts.jsx
+++ nft-page2/src/components/Nfts.jsx
@@ -1,7 +1,31 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Nfts = ({ page }) => {
   const [selectedPage, setSelectedPage] = useState(1);
+  const [nfts, setNfts] = useState();
+
+  const getNfts = async (p) => {
+    try {
+      let nftArray = [];
+
+      setNfts();
+
+      for (let i = 0; i < 10; i++) {
+        const tokenId = i + 1 + (p - 1) * 10;
+
+        let response = await axios.get(
+          `https://olbm.mypinata.cloud/ipfs/QmU52T5t4bXtoUqQYStgx39DdXy3gLQq7KDuF1F9g3E9Qy/${tokenId}.json`
+        );
+
+        nftArray.push({ tokenId, metadata: response.data });
+      }
+
+      setNfts(nftArray);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const pageComp = () => {
     let pageArray = [];
@@ -13,16 +37,25 @@ const Nfts = ({ page }) => {
             i + 1 === selectedPage ? "text-black" : "text-gray-500"
           }`}
           key={i}
+          onClick={() => {
+            setSelectedPage(i + 1);
+          }}
         >
           {i + 1} 페이지
         </button>
       );
     }
+
     return pageArray;
   };
 
+  useEffect(() => {
+    getNfts(1);
+  }, []);
+
+  useEffect(() => console.log(nfts), [nfts]);
+
   return (
-    //
     <div>
       <div>{pageComp()}</div>
     </div>
