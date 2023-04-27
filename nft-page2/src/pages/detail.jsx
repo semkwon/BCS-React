40eaa97f5f07dfda0719514ec23d5bc9b7f9446f
semkwon <3pkwon@gmail.com>
2023-04-27 09:48:19 +0900

    detail 정보 가져오기

--- nft-page2/src/pages/detail.jsx
+++ nft-page2/src/pages/detail.jsx
@@ -1,4 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const Detail = () => {
+  const [metadata, setMetadata] = useState();
+
+  const { tokenId } = useParams();
+
+  const getNft = async () => {
+    try {
+      const response = await axios.get(
+        `https://olbm.mypinata.cloud/ipfs/QmU52T5t4bXtoUqQYStgx39DdXy3gLQq7KDuF1F9g3E9Qy/${tokenId}.json`
+      );
+
+      setMetadata(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getNft();
+  }, []);
+
+  useEffect(() => console.log(metadata), [metadata]);
+
   return <div>Detail</div>;
 };
 
